c4a4343ec5b82673a4ccde702f1cef230eb4fa24
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-03 02:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function happyHolidays(){
 
 happyHolidays();
 
-function happyHolidaysTo(holiday, name){
-  return 'Happy ${holidays}, ${name}';
+function happyHolidaysTo(name){
+  return 'Happy holidays, ${name}!';
 }
 
 happyHolidaysTo();
