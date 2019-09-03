a0980ae56b51530b422765ae0b1b2c71b6069066
Kimberly M Semenza <ksemenza13@gmail.com>
2019-09-03 02:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// write your code below!
+let name = 'you'
+let holida = 'Independence Day';
+let number = 20;
+let mother = "Mother's Day";
+
+function happyHolidays(){
+  return "Happy holiday!";
+}
+
+happyHolidays();
+
+function happyHolidaysTo(name){
+  return 'Happy ${holiday}, ${name}!';
+}
+
+happyHolidaysTo();
+
+function holidayCountdown(mother, number){
+  return 'Its ${number} days until ${mother}!';
+}
+holidayCountdown();
